5699d1a3a572c454ca84425989385e2813957e54
gios <pblazhchuk@gmail.com>
2016-01-14 16:00:23 +0200

    remove babel-register

--- server/index.js
+++ server/index.js
@@ -1,2 +1,16 @@
-require('babel-register')
-require('./server.js')
+"use strict";
+const express = require('express')
+
+let app = new express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/../dist'))
+app.set('view engine', 'html')
+
+app.get('/api/hello', function (req, res) {
+  res.send('Hello World!');
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Convey is running on port', app.get('port'))
+});
