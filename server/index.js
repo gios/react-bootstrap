2f87a363f90dd69b3a1e61357e4f63801541b1e3
gios <pblazhchuk@gmail.com>
2016-01-14 16:09:42 +0200

    rename prod folder

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 let app = new express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/../dist'))
+app.use(express.static(__dirname + '/../convey'))
 app.set('view engine', 'html')
 
 app.get('/api/hello', function (req, res) {
