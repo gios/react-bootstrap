a2188d9c8a3b7fb1a2cf5153f42d07fc9b425926
gios <pblazhchuk@gmail.com>
2016-01-19 17:43:27 +0200

    move to koa

--- server/index.js
+++ server/index.js
@@ -1,30 +1,15 @@
 "use strict";
-const express = require('express')
-const pg = require('pg')
+const app = require('koa')()
+const router = require('koa-router')()
+const serve = require('koa-static')
 
-let app = new express()
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/../public'))
-app.set('view engine', 'html')
-
-app.get('/api/hello', function (req, res) {
-  res.send('Hello World!');
+router.get('/api/hello', function *() {
+  this.body = 'Hello World!';
 })
 
-app.get('/db', function (req, res) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err) {
-        res.send("Error " + err)
-      } else {
-        res.send(result.rows)
-      }
-    })
-  })
-})
+app.use(router.routes())
+app.use(router.allowedMethods())
+app.use(serve(__dirname + '/../public'))
 
-app.listen(app.get('port'), function() {
-  console.log('Top Request is running on port', app.get('port'))
-});
+app.listen(process.env.PORT || 5000);
+console.log('Top Request is running on port', process.env.PORT || 5000)
