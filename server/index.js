86d3ea8a09295eb0fb974b645f8747d179727a79
Pavlo Blazhchuk <gios@users.noreply.github.com>
2016-01-19 16:41:45 +0200

    update

--- server/index.js
+++ server/index.js
@@ -26,5 +26,5 @@ app.get('/db', function (req, res) {
 })
 
 app.listen(app.get('port'), function() {
-  console.log('Convey is running on port', app.get('port'))
+  console.log('Top Request is running on port', app.get('port'))
 });
