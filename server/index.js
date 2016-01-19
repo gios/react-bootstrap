f80dd7afb77f7339ecf446f73a9cadecd823e7b6
gios <pblazhchuk@gmail.com>
2016-01-19 18:01:23 +0200

    add jwt koa module

--- server/index.js
+++ server/index.js
@@ -2,15 +2,16 @@
 const app = require('koa')()
 const router = require('koa-router')()
 const serve = require('koa-static')
+const jwt = require('koa-jwt')
 
 router.get('/api/hello', function *() {
-  this.body = 'Hello World!';
+  this.body = 'Hello World!'
 })
 
 app.use(router.routes())
 app.use(router.allowedMethods())
 app.use(serve(__dirname + '/../public'))
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000)
 
 console.log('Top Request is running on port', process.env.PORT || 5000) // eslint-disable-line no-console
