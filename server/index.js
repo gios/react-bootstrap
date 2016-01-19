c2d2e8f0021eae5f0c82d9841b1ee38cc635c5a3
gios <pblazhchuk@gmail.com>
2016-01-19 18:06:24 +0200

    add bodyparse

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const app = require('koa')()
 const router = require('koa-router')()
 const serve = require('koa-static')
+const bodyParser = require('koa-bodyparser')
 const jwt = require('koa-jwt')
 
 router.get('/api/hello', function *() {
@@ -10,6 +11,7 @@ router.get('/api/hello', function *() {
 
 app.use(router.routes())
 app.use(router.allowedMethods())
+app.use(bodyParser())
 app.use(serve(__dirname + '/../public'))
 
 app.listen(process.env.PORT || 5000)
